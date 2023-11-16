8f81849c0ca43a3cedc4e14dd130ce6b2ac3870f
NahuelSaturi <lucasnahu16@gmail.com>
2023-11-16 01:31:58 -0300

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+export default function Card(props) {
+   // id={Rick.id}
+   // name={Rick.name}
+   // status={Rick.status}
+   // species={Rick.species}
+   // gender={Rick.gender}
+   // origin={Rick.origin.name}
+   // image={Rick.image}
+   // onClose={() => window.alert('Emulamos que se cierra la card')}
+   return (
+      <div>
+         <button onClick={props.onClose}>X</button>
+         <h2>{props.name}</h2>
+         <h4>Id: {props.id}</h4>
+         <h4>Status: {props.status}</h4>
+         <h4>Specie: {props.spicies}</h4>
+         <h4>Gender: {props.gender}</h4>
+         <h4>Origin: {props.origin}</h4>
+         <img src={props.image} alt={props.name} />
+      </div>
+   );
+}
