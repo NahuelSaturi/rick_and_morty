5c92693e82f8af2c42a72ab1d2f2d8f75837b15c
NahuelSaturi <lucasnahu16@gmail.com>
2023-11-29 14:24:06 -0300

    creamos carpeta cliente y server

---
+++ client/prueba/index.jsx
@@ -0,0 +1,6 @@
+const { useState } = require("react");
+
+const [value, setValue] = useState('')
+const handleChange = (event) => setValue.event.target.toUpperCase()
+
+return <input type="text"></input>
